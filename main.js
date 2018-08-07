961db4b3696c022dba1577e3dda409c7d58a4bce
Matt N <mattnappo@gmail.com>
2018-08-06 22:42:15 -0400

    Adding logging

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const Discord = require('discord.js');
+var fs = require('fs');
 const config = require('./auth.json');
 
 const client = new Discord.Client();
+
+var epoch = Math.round((new Date).getTime() / 100);
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -10,10 +13,21 @@ client.on('ready', () => {
 client.login(config.token);
 
 client.on('message', message => {
+    var log = message.content;
+    var file = "./logs/" + epoch + ".log";
+    fs.writeFile(file, log, function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log(log + " logged in " + file);
+    }); 
     var lowerMsg = message.content.toLowerCase();
+    if (lowerMsg == "alexa") {
+        message.channel.send("How may I help you? (hint: I can play music!)");
+    }
     if (lowerMsg.includes("alexa") && lowerMsg.includes("play")) {
 
-        var key = "alexa";
+        var key = "";
         if (lowerMsg.includes("alexa, play ")) { key = "alexa, play " }
         if (lowerMsg.includes("alexa, play")) { key = "alexa, play" }
         if (lowerMsg.includes("alexa play ")) { key = "alexa play " }
@@ -33,6 +47,8 @@ client.on('message', message => {
                 { time: 10000 }
             );
 
+            console.log(collector);
+
             collector.on('collect', message => {
                 if (message.content.toLowerCase() == "yes" || message.content.toLowerCase() == "y") {
                     message.channel.send("I don't care. I'm putting on some better music.")
@@ -42,14 +58,20 @@ client.on('message', message => {
             });
 
         } else {
-            message.channel("Ah! I see that you have some good taste!");
+            message.channel.send("Ah! I see that you have some good taste!");
         }
         
         var msg_header = "Now playing: Despacito by God";
         message.channel.send(msg_header);
-        
+
         var options = "───────────⚪────── ◄◄⠀▐▐ ⠀►►⠀ 3:08 / 4:42 ⠀ ───○ 🔊 ᴴᴰ ⚙️";
         message.channel.send(options);
 
     }
 });
+
+function clean_logs() {
+    
+}
+
+clean_logs();
