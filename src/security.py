15408e819bd24e8ac3aaf5870ef66a5bfa0ee595
Matt N <mattnappo@gmail.com>
2018-08-28 14:26:03 -0400

    Updated key security

--- src/security.py
+++ src/security.py
@@ -1,7 +1,9 @@
 import json
-key = "enter-key-here"
+key = ""
 encrypted_key = []
 for char in key:
     encrypted_key.append(char)
 with open("auth.json", "w") as json_file:
     json.dump(encrypted_key, json_file)
+
+print(encrypted_key)
